7c5a76dd7daebd1659405535abc62baa3c00f434
Nada Rabea <nada_rabea@yahoo.com>
2018-08-05 20:12:34 +0200

    Change route of user index endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
     mount_devise_token_auth_for 'User', at: 'auth'  
-    get 'users/index'  
+    get 'users' => 'users#index'  
   end
 end
